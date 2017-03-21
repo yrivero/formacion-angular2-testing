10989ad56a2ee200df30957559fde2b97e4694cd
XYZ <idm.caz@mailinator.com>
2017-03-21 09:45:25 +0100

    Initial workbench commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,10 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
+
+
+
+
+
+
 });
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,13 @@
 /* SystemJS module definition */
-declare var module: {
-  id: string;
-};
+
+/*
+ Seems to have syntax errors.
+ Makes the TypeScript compilation step fail.
+ Commented out until we have need for it.
+ */
+
+/*
+ declare var module: {
+ id: string;
+ };
+ */
