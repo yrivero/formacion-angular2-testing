4450dc6f3424634470298bbc1d5b429b4505b58f
XYZ <idm.caz@mailinator.com>
2017-03-22 10:59:28 +0100

    Removed unnecessary declarations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { StandaloneButtonComponent } from './isolated-unit-tests/standalone-components/standalone-button.component';
-import { MultiplierPanelComponent } from "./isolated-unit-tests/components-with-io/multiplier-panel.component";
-import {MultiplierButtonComponent} from "./isolated-unit-tests/components-with-io/multiplier-button.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [
-    StandaloneButtonComponent,
-    MultiplierButtonComponent,
-    MultiplierPanelComponent
-
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'The application is working!';
